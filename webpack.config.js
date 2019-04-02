6ebc36456fd8a8d44e495b80a30629e59fb019b8
tamakiii <tamakiii@users.noreply.github.com>
2019-04-02 20:07:17 +0900

    \t -> two-spaces

--- webpack.config.js
+++ webpack.config.js
@@ -25,50 +25,50 @@ const path = require('path');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-	module: {
-		rules: [
-			{
-				include: [path.resolve(__dirname, 'src')],
-				loader: 'babel-loader',
+  module: {
+    rules: [
+      {
+        include: [path.resolve(__dirname, 'src')],
+        loader: 'babel-loader',
 
-				options: {
-					plugins: ['syntax-dynamic-import'],
+        options: {
+          plugins: ['syntax-dynamic-import'],
 
-					presets: [
-						[
-							'@babel/preset-env',
-							{
-								modules: false
-							}
-						]
-					]
-				},
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                modules: false
+              }
+            ]
+          ]
+        },
 
-				test: /\.js$/
-			}
-		]
-	},
+        test: /\.js$/
+      }
+    ]
+  },
 
-	output: {
-		chunkFilename: '[name].[chunkhash].js',
-		filename: '[name].[chunkhash].js'
-	},
+  output: {
+    chunkFilename: '[name].[chunkhash].js',
+    filename: '[name].[chunkhash].js'
+  },
 
-	mode: 'development',
+  mode: 'development',
 
-	optimization: {
-		splitChunks: {
-			cacheGroups: {
-				vendors: {
-					priority: -10,
-					test: /[\\/]node_modules[\\/]/
-				}
-			},
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendors: {
+          priority: -10,
+          test: /[\\/]node_modules[\\/]/
+        }
+      },
 
-			chunks: 'async',
-			minChunks: 1,
-			minSize: 30000,
-			name: true
-		}
-	}
+      chunks: 'async',
+      minChunks: 1,
+      minSize: 30000,
+      name: true
+    }
+  }
 };
